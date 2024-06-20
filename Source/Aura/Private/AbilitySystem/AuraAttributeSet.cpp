d327a60390b5eba9f4b261e5a21331e17e572bca
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-06-20 18:16:36 +0200

    Added AuraAbilitySystemComponent, AuraAttributeSet and AuraPlayerState. Add variables to them in AuraEnemy and AuraPlayerState

---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Gustavo Ardaya
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,16 @@
+// Copyright Gustavo Ardaya
+
+
+#include "Player/AuraPlayerState.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+
+	AbilitySystemComponent = CreateDefaultSubobject<UAuraAbilitySystemComponent>("AbilitySystemComponent");
+	AbilitySystemComponent->SetIsReplicated(true);
+
+	AttributeSet = CreateDefaultSubobject<UAuraAttributeSet>("AttributeSet");
+}
