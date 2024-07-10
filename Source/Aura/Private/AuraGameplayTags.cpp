f696e74ba286964fcecf8d12b6f4ab5d2536f56e
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-07-10 17:15:45 +0200

    Add AuraGameplayTags singleton struct

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,12 @@
+// Copyright Gustavo Ardaya
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secundary.Armor"), FString("Reduces damage taken, improves Block Chaance"));
+}
