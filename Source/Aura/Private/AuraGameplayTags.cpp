0fff79f71f317e42b7de90bc23bf25f1ed6914ec
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-07-10 17:33:22 +0200

    Add AuraAssetManager singleton class

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -8,5 +8,5 @@ FAuraGameplayTags FAuraGameplayTags::GameplayTags;
 
 void FAuraGameplayTags::InitializeNativeGameplayTags()
 {
-	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secundary.Armor"), FString("Reduces damage taken, improves Block Chaance"));
+	GameplayTags.Attributes_Secondary_Armor = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Secundary.Armor"), FString("Reduces damage taken, improves Block Chaance"));
 }
