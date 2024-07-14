68b8e921bf51946c936e1b6f13be5ade208a2a19
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-07-14 16:57:12 +0200

    add Attribute Info Delegate to TextValueButton and AttributeMenu blueprints

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,13 +2,23 @@
 
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AuraGameplayTags.h"
 
-void UAttributeMenuWidgetController::BroadcastInitialValues()
-{
-
-}
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
 {
 
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
+
+	check(AttributeInfo);
+
+	FAuraAttributeInfo Info = AttributeInfo->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strenght);
+	
+	Info.AttributeValue = AS->GetStrength();
+	AttributeInfoDelegate.Broadcast(Info);
 }
--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -4,7 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/AuraWidgetController.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
 #include "AttributeMenuWidgetController.generated.h"
+
+//struct FAuraAttributeInfo;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAttributeInfoSignature, const FAuraAttributeInfo, Info);
 
 /**
  * 
@@ -18,5 +23,13 @@ public:
 
 	virtual void BroadcastInitialValues() override;
 	virtual void BindCallbacksToDependencies() override;
+
+	UPROPERTY(BlueprintAssignable, Category="GAS|Attributes")
+	FAttributeInfoSignature AttributeInfoDelegate;
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UAttributeInfo> AttributeInfo;
 	
 };
