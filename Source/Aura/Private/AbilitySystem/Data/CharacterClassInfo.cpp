b6e1991fbb448e79481455aa60fadbc141ef2568
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-08-20 11:22:47 +0200

    Add CharacterClassInfo Data Asset

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Gustavo Ardaya
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
