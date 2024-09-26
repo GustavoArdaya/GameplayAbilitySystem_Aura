0c72ae206ec28b5bf7a4c9aaf070258899fdfe18
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-09-26 14:58:32 +0200

    add AI controller and BehaviorTree

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,15 @@
+// Copyright Gustavo Ardaya
+
+
+#include "AI/AuraAIController.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "BehaviorTree/BehaviorTreeComponent.h"
+
+AAuraAIController::AAuraAIController()
+{
+	Blackboard = CreateDefaultSubobject<UBlackboardComponent>("BlackboardComponent");
+	check(Blackboard);
+	BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTreeComponent");
+	check(BehaviorTreeComponent);
+
+}
