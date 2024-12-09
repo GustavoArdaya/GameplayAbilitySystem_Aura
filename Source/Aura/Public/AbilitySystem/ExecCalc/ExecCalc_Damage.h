f37be244ee01c6a38d3d5daa2460e91187d0cef6
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-12-09 19:10:26 +0100

    Take Debuff into account in ExecCalc_Damage

--- Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
+++ Source/Aura/Public/AbilitySystem/ExecCalc/ExecCalc_Damage.h
@@ -20,4 +20,5 @@ public:
 	UExecCalc_Damage();
 
 	virtual void Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams, FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const override;
+	void DetermineDebuff(const FGameplayEffectSpec& Spec, const FGameplayEffectCustomExecutionParameters& ExecutionParams, FAggregatorEvaluateParameters& EvaluationParameters, const TMap<FGameplayTag, FGameplayEffectAttributeCaptureDefinition>& InTagsToDefs) const;
 };
