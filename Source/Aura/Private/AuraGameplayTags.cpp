ee74da0248743ebd3a0ec8c30f7c7c6d8c118629
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2024-12-31 11:56:17 +0100

    Add ArcaneShards BP

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -286,7 +286,7 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	);
 
 
-	// Spell Abilities
+	// Offensive Spell Abilities
 
 	GameplayTags.Abilities_Fire_FireBolt = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Fire.FireBolt"),
@@ -296,6 +296,11 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	GameplayTags.Abilities_Lightning_Electrocute = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.Lightning.Electrocute"),
 		FString("Electrocute Ability Tag")
+	);
+
+	GameplayTags.Abilities_Arcane_ArcaneShards = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Arcane.ArcaneShards"),
+		FString("Arcane Shards Ability Tag")
 	);
 
 	// Passive Spell Abilities
