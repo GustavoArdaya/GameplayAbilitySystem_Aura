260e12632eb19c557fa0e42e142d80889c162533
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-01-07 13:49:44 +0100

    Spawn fireballs in FireBlast

---
+++ Source/Aura/Private/Actor/AuraFireBall.cpp
@@ -0,0 +1,14 @@
+// Copyright Gustavo Ardaya
+
+
+#include "Actor/AuraFireBall.h"
+
+void AAuraFireBall::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AAuraFireBall::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+
+}
---
+++ Source/Aura/Public/Actor/AuraFireBall.h
@@ -0,0 +1,23 @@
+// Copyright Gustavo Ardaya
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Actor/AuraProjectile.h"
+#include "AuraFireBall.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraFireBall : public AAuraProjectile
+{
+	GENERATED_BODY()
+	
+public:
+
+protected:
+
+	virtual void BeginPlay() override;
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
+};
