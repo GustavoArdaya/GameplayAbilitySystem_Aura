dca3811a857312e23d7bfbff015e76064927ee45
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-01-15 10:20:22 +0100

    Bind Variable PlayerName to ViewModel

--- Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
+++ Source/Aura/Private/UI/ViewModel/MVVM_LoadSlot.cpp
@@ -5,5 +5,20 @@
 
 void UMVVM_LoadSlot::InitializeSlot()
 {	
-	SetWidgetSwitcherIndex.Broadcast(1);
+	SetWidgetSwitcherIndex.Broadcast(2);
+}
+
+void UMVVM_LoadSlot::SetLoadSlotName(FString InLoadSlotName)
+{
+	UE_MVVM_SET_PROPERTY_VALUE(LoadSlotName, InLoadSlotName);
+}
+
+void UMVVM_LoadSlot::SetPlayerName(FString InPlayerName)
+{
+	UE_MVVM_SET_PROPERTY_VALUE(PlayerName, InPlayerName);
+}
+
+void UMVVM_LoadSlot::SetSlotIndex(int32 InSlotIndex)
+{
+	UE_MVVM_SET_PROPERTY_VALUE(SlotIndex, InSlotIndex);
 }
--- Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
+++ Source/Aura/Public/UI/ViewModel/MVVM_LoadSlot.h
@@ -22,5 +22,27 @@ public:
 	FSetWidgetSwitcherIndex SetWidgetSwitcherIndex;
 
 	void InitializeSlot();
+
 	
+
+	// Field Notifies
+
+	void SetLoadSlotName(FString InLoadSlotName);
+	void SetPlayerName(FString InPlayerName);
+	void SetSlotIndex(int32 InSlotIndex);
+
+	FString GetLoadSlotName() const { return LoadSlotName; }
+	FString GetPlayerName() const { return PlayerName; }
+	int32 GetSlotIndex() const { return SlotIndex; }
+
+private:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess = "true"));
+	FString LoadSlotName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess = "true"));
+	FString PlayerName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, FieldNotify, Setter, Getter, meta = (AllowPrivateAccess = "true"));
+	int32 SlotIndex;
 };
