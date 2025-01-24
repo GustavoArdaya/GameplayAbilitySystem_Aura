016c1dcc0cd56a2813441053b5cb484bcb862aa6
Gustavo.Ardaya <gustavo.ardaya@gmail.com>
2025-01-24 11:25:39 +0100

    Add Map Entrance

---
+++ Source/Aura/Private/Checkpoint/MapEntrance.cpp
@@ -0,0 +1,39 @@
+// Copyright Gustavo Ardaya
+
+
+#include "Checkpoint/MapEntrance.h"
+#include "Components/SphereComponent.h"
+#include <Interaction/PlayerInterface.h>
+#include <Game/AuraGameModeBase.h>
+#include <Kismet/GameplayStatics.h>
+
+AMapEntrance::AMapEntrance(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	Sphere->SetupAttachment(MoveToComponent);
+}
+
+void AMapEntrance::LoadActor_Implementation()
+{
+	// Do nothing when loading map entrance
+}
+
+void AMapEntrance::HighlightActor_Implementation()
+{
+	CheckpointMesh->SetRenderCustomDepth(true);
+}
+
+void AMapEntrance::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->Implements<UPlayerInterface>())
+	{
+		bReached = true;
+
+		if (AAuraGameModeBase* AuraGM = Cast<AAuraGameModeBase>(UGameplayStatics::GetGameMode(this)))
+		{
+			AuraGM->SaveWorldState(GetWorld(), DestinationMap.ToSoftObjectPath().GetAssetName());
+		}
+		IPlayerInterface::Execute_SaveProgress(OtherActor, DestinationPlayerStartTag);
+
+		UGameplayStatics::OpenLevelBySoftObjectPtr(this, DestinationMap);
+	}
+}
--- Source/Aura/Public/Game/AuraGameModeBase.h
+++ Source/Aura/Public/Game/AuraGameModeBase.h
@@ -38,7 +38,7 @@ public:
 
 	void SaveInGameProgressData(ULoadScreenSaveGame* SaveObject);
 
-	void SaveWorldState(UWorld* World) const;
+	void SaveWorldState(UWorld* World, const FString& DestinationMapAssetName = FString("")) const;
 	void LoadWorldState(UWorld* World) const;
 
 	void TravelToMap(UMVVM_LoadSlot* Slot);
@@ -57,6 +57,8 @@ public:
 
 	UPROPERTY(EditDefaultsOnly)
 	TMap<FString, TSoftObjectPtr<UWorld>> Maps;
+
+	FString GetMapNameFromMapAssetName(const FString& MapAssetName) const;
 
 	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override; 
 
